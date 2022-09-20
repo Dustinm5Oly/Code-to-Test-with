e1c8fa9aef8cdff3124233a047cc14ba4b83d2e0
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-09-20 09:22:12 -0700

    added initial html, started timer, question and answer variables

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const setTimer = document.querySelector("#time");
+let secondsLeft = 30;
+let questions = [
+    "question1",
+    "question2",
+    "question3",
+    "question4",
+];
+let answers = [
+    ["answer1", "answer2", "answer3", "answer4"],
+    ["answer5", "answer6", "answer7", "answer8"],
+    ["answer9", "answer10", "answer11", "answer12"],
+    ["answer13", "answer14", "answer15", "answer16"],
+];
+
+
+function setTime() {
+    var timer = document.createElement("p");
+    setTimer.appendChild(timer)
+    var timerInterval= setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft + "Seconds Left";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+setTime();
