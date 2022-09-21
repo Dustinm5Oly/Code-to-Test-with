4428dde589765a3f6a078001689ab28fe1e01dae
Dustinm5Oly <111804479+Dustinm5Oly@users.noreply.github.com>
2022-09-21 09:20:48 -0700

    was able to make the new questions and answers populate from one to the next, update the timer to follow along and started with the high scores section

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 const setTimer = document.querySelector("#time");
 const startQuiz= document.getElementById("Start-Quiz")
+const header = document.getElementById("header");
+const text = document.getElementById("text");
+const questionsEl = document.querySelector("#question");
+const answerEl0 = document.getElementById("answer0");
+const answerEl1 = document.getElementById("answer1");
+const answerEl2 = document.getElementById("answer2");
+const answerEl3 = document.getElementById("answer3");
+const questionsDiv = document.querySelector(".questions");
+const answerButton = [answerEl0, answerEl1, answerEl2, answerEl3];
+let nextQuestion = 0
+const highScoreDiv= document.querySelector(".highscores");
+
 let secondsLeft = 30;
 let questions = [
     "question1",
@@ -14,8 +26,48 @@ let answers = [
     ["answer13", "answer14", "answer15", "answer16"],
 ];
 
+for (let i = 0; i < answerButton.length; i++) {
+    answerButton[i].addEventListener('click', function(e){
+        console.log(e.target.innerText);
+        const selectedAnswer = e.target.innerText;
+        nextQuestion++;
+        if (nextQuestion > 3){
+        showHighScore();
+        } else {
+        displayQuestion();
+        }
+    });
+}
+
+function showHighScore(){
+    questionsDiv.setAttribute("style", "display:none");
+    highScoreDiv.setAttribute("style", "display:block");
+}
+
+function displayQuestion(){
+
+    //for (let i = 0; i < questions.length; i++) {
+        questionsEl.textContent=questions[nextQuestion];
+        answerEl0.textContent=answers[nextQuestion][0];
+        answerEl1.textContent=answers[nextQuestion][1];
+        answerEl2.textContent=answers[nextQuestion][2];
+        answerEl3.textContent=answers[nextQuestion][3];
+
+
+        
+   // }
+}
+
+// function answerQuestion(){
+
+// }
+
 
 function setTime() {   
+    header.setAttribute("style", "display:none");
+    text.setAttribute("style", "display:none"); 
+    startQuiz.setAttribute("style", "display:none");
+    questionsDiv.setAttribute("style", "display:block");
     var timer = document.createElement("p");
     setTimer.appendChild(timer)
     var timerInterval= setInterval(function() {
@@ -28,4 +80,7 @@ function setTime() {
     }, 1000);
 }
 
-startQuiz.addEventListener("click", setTime);
+startQuiz.addEventListener("click", function(){
+    setTime();
+    displayQuestion();
+});
